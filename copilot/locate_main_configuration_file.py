4f681c0f75d1bb96355d21ad870c53bc0953c919
John De Armas <dearmas.john1996@gmail.com>
2020-12-08 18:12:47 -0700

    locate main configuration file
    dirty: cont: add windows support

--- copilot/locate_main_configuration_file.py
+++ copilot/locate_main_configuration_file.py
@@ -1,3 +1,27 @@
+#!/bin/env python
 # locate_main_configuration_file.py: locate apache's main configuration file
 
-default_filename = "http.conf"
+import os
+
+filename = "httpd.conf"
+
+# determine what os were are on
+roots = {
+    "posix":"/",
+    "nt":"c:/"
+}
+
+root = roots[os.name]
+
+
+
+def find_files(filename, search_path):
+    result = []
+
+    # Walking top-down from the root
+    for root, dir, files in os.walk(search_path):
+        if filename in files:
+            result.append(os.path.join(root, filename))
+            return result
+
+print(find_files(filename,root))
